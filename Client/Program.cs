fde29120d124918fcd46ed7ceac8117a0a53a0f6
mohsen afshar <mohsenafshar_ph@yahoo.com>
2020-03-10 19:46:26 +0330

    Initial Commit

---
+++ Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // CreateSecondClient();
+            StartClient();
+            
+            Console.ReadLine();
+        }
+
+        private static async void CreateSecondClient()
+        {
+            
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("localhost", 1883)
+                .Build();
+            
+            var mqttClientAuthenticateResult = client.ConnectAsync(options).Result;
+        }
+
+        private static async void StartClient()
+        {
+            // Create a new MQTT client.
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            
+            var options = new MqttClientOptionsBuilder()
+                // .WithClientId("Client1")
+                .WithTcpServer("localhost", 1883)
+                .WithCredentials("Hans", "Test")
+                // .WithTls()
+                .WithCleanSession()
+                .Build();
+            
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                // Subscribe to a topic
+                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("test/topic").Build());
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+            
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+
+                Task.Run(() => mqttClient.PublishAsync("hello/world"));
+            });
+            
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("test/topic")
+                .WithPayload("1")
+                .WithExactlyOnceQoS()
+                .WithRetainFlag()
+                .Build();
+            
+
+            await mqttClient.ConnectAsync(options);
+            await mqttClient.PublishAsync(message);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MQTTnet;
+using MQTTnet.Server;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //
+            // var mqttFactory = new MqttFactory();
+            // var client = mqttFactory.CreateMqttClient();
+            //
+            // var options = new MqttClientOptionsBuilder()
+            //     .WithClientId(Guid.NewGuid().ToString())
+            //     .WithTcpServer("tcp://192.168.1.190:1883")
+            //     .Build();
+            //
+            // var mqttClientAuthenticateResult = client.ConnectAsync(options).Result;
+
+            StartServer();
+
+        }
+
+        private static async void StartServer()
+        {
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            await mqttServer.StartAsync(new MqttServerOptions());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+            await mqttServer.StopAsync();
+        }
+    }
+}
